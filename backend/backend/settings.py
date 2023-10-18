9a28a564e5664fa57df7f77e4757613cf78dcf1a
ntp0102 <nguyenthienphuc261100@gmail.com>
2023-10-18 16:09:24 +0700

    config deploy backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -186,13 +186,15 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_DIRS = [
     BASE_DIR/'static',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
