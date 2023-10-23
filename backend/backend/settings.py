325648da1059a4107ee3cc2d5b98a33883a9607e
ntp0102 <nguyenthienphuc261100@gmail.com>
2023-10-23 23:14:12 +0700

    config deploy backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -36,7 +36,7 @@ DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 # ALLOWED_HOSTS = ['localhost', 'https://website-django-react.vercel.app/']
 
 # ALLOWED_HOSTS = ['*']
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
@@ -53,17 +53,7 @@ CORS_ALLOWED_ORIGINS = [
 #     # os.environ.get("FRONTEND_URL")
 # ]
 # ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-# CORS_ALLOW_HEADERS = [
-#     "accept",
-#     "accept-encoding",
-#     "authorization",
-#     "content-type",
-#     "dnt",
-#     "origin",
-#     "user-agent",
-#     "x-csrftoken",
-#     "x-requested-with",
-# ]
+
 
 # Application definition
 
@@ -128,9 +118,9 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
