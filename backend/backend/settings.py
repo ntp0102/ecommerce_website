319b1d103b73a8e59ee75a739445f6e558af3ad8
ntp0102 <nguyenthienphuc261100@gmail.com>
2023-10-23 23:29:20 +0700

    config deploy backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,18 +25,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-!c+651yr@%88v&!mrvsa9vmq8!7py4@f2nysaz##x+=s@vtg9$'
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-!c+651yr@%88v&!mrvsa9vmq8!7py4@f2nysaz##x+=s@vtg9$'
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = False
+# DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 
 ALLOWED_HOSTS = ['localhost', 'https://website-django-react.vercel.app/']
 
 # ALLOWED_HOSTS = ['*']
-CORS_ALLOW_ALL_ORIGINS = False
+# CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
