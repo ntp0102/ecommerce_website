07f3458f8016ce1e9460d8cba5641ea00d76dfd6
ntp0102 <nguyenthienphuc261100@gmail.com>
2023-10-24 14:23:58 +0700

    config deploy frontend

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,12 +1,12 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// module.exports = function(app) {
-//   app.use(
-//     '/api',
-//     createProxyMiddleware({
-//       // target: 'https://django-ecommerce-render.onrender.com/',
-//       target: 'http://localhost:8000/',
-//       changeOrigin: true,
-//     })
-//   );
-// };
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      // target: 'https://django-ecommerce-render.onrender.com/',
+      target: 'http://localhost:8000/',
+      changeOrigin: true,
+    })
+  );
+};
