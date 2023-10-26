5f587ff54ba9e840ef09ef8a4a2a8a74ebacc020
ntp0102 <nguyenthienphuc261100@gmail.com>
2023-10-26 18:30:51 +0700

    config deploy backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,7 @@ DEBUG = False
 # ALLOWED_HOSTS = ['localhost']
 
 # ALLOWED_HOSTS = ['*']
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
